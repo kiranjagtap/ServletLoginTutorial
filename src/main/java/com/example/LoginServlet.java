09c3daf5843e09fad1c6dfba50ae22229d40e757
Kiran Jagtap <karan27288@fmail.com>
2023-02-05 11:50:36 +0530

    Added code

--- src/main/java/com/example/LoginServlet.java
+++ src/main/java/com/example/LoginServlet.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,14 +11,44 @@ import java.io.PrintWriter;
 
 public class LoginServlet extends HttpServlet {
 
+    private ServletConfig config;
+    private String jdbcUrl = null;
+    private String dbDriver = null;
+    private String adminPath = null;
+    private ServletContext servletContext= null;
+
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("Servlet config initialized");
+        this.config = config;
+        this.init();
+    }
+
+    public void init() throws ServletException {
+        System.out.println("Init called");
+    }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("Post method called");
 
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
+        jdbcUrl = config.getInitParameter("jdbc-url");
+        dbDriver = config.getInitParameter("dbdriver");
+
+        System.out.println("Init Param::JDBC URL  -"+jdbcUrl);
+        System.out.println("Init Param::JDBC Driver  -"+dbDriver);
+
+
+        servletContext = request.getServletContext();
+        adminPath = servletContext.getInitParameter("adminPath");
+
+        System.out.println("CONTEXT Param::adminPath  -"+adminPath);
+
+
         if(username!=null && username.equals("dummy")){
             if(password!=null && password.equals("dummy")){
+
                 PrintWriter out = response.getWriter();
                 out.println("<html>");
                 out.println("<head>");
